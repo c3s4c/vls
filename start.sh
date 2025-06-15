8cb6b132b0f23f03ab7bde384eb80f4a0f94041a
c3s4c <131548301+c3s4c@users.noreply.github.com>
2025-06-15 04:12:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# دانلود آخرین نسخه xray (amd64 لینوکس)
+curl -L -o xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+
+# استخراج فایل
+unzip xray.zip
 chmod +x ./xray
+
+# اجرای xray
 ./xray -config config.json
